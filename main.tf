92869b9cf87b983c4ca622b0410b389ff0b9c2ad
elicarter <89507487+elicarter@users.noreply.github.com>
2024-05-10 08:57:25 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
+resource "aws_security_group" "elb_sg" {
+  vpc_id = var.existing_vpc_id
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
